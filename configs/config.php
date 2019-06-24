8bd7d82aaa2e07956ad249c7ad35c9b6d743b1bb
AltFourTwo <52165827+AltFourTwo@users.noreply.github.com>
2019-06-24 10:30:57 -0400

    Create config.php

---
+++ configs/config.php
@@ -0,0 +1,18 @@
+<?php
+    interface Config
+    {
+        const DB_HOST = "localhost";
+        const DB_USER = "root";
+        const DB_PWD  = "root";
+        const DB_NAME = "LOUETAPLACE";
+        
+        
+        const DB_TABLE_SALLE = "salle";
+        const DB_TABLE_DISPO = "dispo";
+        const DB_TABLE_USER = "utilisateur";
+        const DB_TABLE_LOC = "location";
+        const DB_TABLE_SALFURN = "salle_has_furniture";
+        const DB_TABLE_FURN = "fourniture";
+        const DB_TABLE_SALEQ = "salle_has_equipement";
+        const DB_TABLE_EQUIP = "equipement";
+    }
