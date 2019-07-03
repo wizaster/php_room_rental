cdecc921a5991dffcb3773a8f2b820cc7a38d95a
AltFourTwo <52165827+AltFourTwo@users.noreply.github.com>
2019-07-03 15:45:08 -0400

    Add files via upload

--- vues/profil.php
+++ vues/profil.php
@@ -56,8 +56,8 @@ include('header.php');
     </div>
 </div>
 <div class="site-section bg-light ">
-    <div class="container row col-12 profil">
-        <div class=" m-auto">
+    <div class="container row col-4 profil">
+        <div class="m-auto">
             <fieldset>
                 <legend>Informations personnelles</legend>
                 <div>Nom Utilisateur :</div>
@@ -82,33 +82,60 @@ include('header.php');
                 <div><?php echo $_SESSION['user']['bio'] ?></div>
                 <br/>
             </fieldset>
+            <a href="?action=info_utilisateur">Modifier Profil :)</a>
         </div>
-        <div class="col-6">
-            <input type="hidden" value="action"
-            <?php
-            switch ($_SESSION['role']) {
-                case 1:
-                    ?>
-                    <input type="button" value="consulter historique"/>
-                    <input type="button" value="Editer utilisateur"/>
-                    <input type="button" value="Editer salle"/>
+        <div>
+            <div class="m-auto">
+                <div class="row">
+                    <form action="#" method="post" id="Options" class="p-2">
+                        <input type="submit" value="Consulter Historique"></input>
+                        <input type="hidden" name="action" value="consulter_historique"/>
+                    </form>
+                </div>
+                <div class="row">
+                    <form action="#" method="post" id="Options" class="p-2">
+                        <input type="submit" value="Editer Profil"></input>
+                        <input type="hidden" name="action" value="info_utilisateur"/>
+                    </form>
+                </div>
                     <?php
-                    break;
-                case 2:
-                    ?>
-                    <input type="button" value="modifier salle"/>
-                    <input type="button" value="modifer profil"/>
-                    <input type="button" value="Ajouter salle"/>
-                    <?php
-                    break;
-                case 3:
-                    ?>
-                    <input type="button" value="devenir proprietaire"/>
-                    <input type="button" value="modifer profil"/>
-                <?php
-            }
-            ?>
+                    switch ($_SESSION['role']) {
+                        case 1:
+                            ?>
+                            <div class="row">
+                                <form action="#" method="post" id="Options" class="p-2">
+                                    <input type="submit" value="Devenir Proprietaire"/>
+                                    <input type="hidden" name="action" value="devenir_proprietaire"/>
+                                </form>
+                            </div>
+                            <?php
+                            break;
+                        case 2:
+                            ?>
+                            <div class="row">
+                                <form action="#" method="post" id="Options" class="p-2">
+                                    <input type="submit" value="Devenir Proprietaire"/>
+                                    <input type="submit" value="Ajouter salle"/>
+                                    <input type="hidden" name="action" value="ajouter_salle"/>
+                                </form>
+                            </div>
+                            <div class="row">
+                                <form action="#" method="post" id="Options" class="p-2">
+                                    <input type="submit" value="Modifier Salle"/>
+                                    <input type="hidden" name="action" value="modifier_salle"/>
+                                </form>
+                            </div>
+                            <?php
+                            break;
+                        case 3:
+                            ?>
 
+                            <?php
+                            break;
+                    }
+                    ?>
+                </form>
+            </div>
         </div>
     </div>
 </div>
